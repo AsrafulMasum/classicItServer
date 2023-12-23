0a9804b30d40653c67671d0e7de474a227455b8c
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2023-12-23 23:47:19 +0600

    require api created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const cors = require("cors");
@@ -18,14 +18,14 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1oqnk0p.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1oqnk0p.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 // jwt middleware
@@ -56,6 +56,8 @@ dbConnect();
 
 const database = client.db("classicItTaskDB");
 const usersCollections = database.collection("usersDB");
+const productsCollections = database.collection("productsDB");
+const cartsCollections = database.collection("cartsDB");
 
 // jwt api method
 app.post("/jwt", (req, res) => {
@@ -99,6 +101,33 @@ app.post("/users", async (req, res) => {
     const result = await usersCollections.insertOne(user);
     res.send(result);
   }
+});
+
+// products api method
+app.get("/products", async (req, res) => {
+  const cursor = productsCollections.find();
+  const result = await cursor.toArray();
+  res.send(result);
+});
+
+app.get("/products/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await productsCollections.findOne(query)
+  res.send(result);
+});
+
+app.post("/carts", verifyCookie, async (req, res) => {
+  const cartInfo = req.body;
+  const result = await cartsCollections.insertOne(cartInfo);
+  res.send(result);
+});
+
+app.get("/carts/:email", async (req, res) => {
+  const userEmail = req.params.email;
+  const query = { email: userEmail };
+  const result = await cartsCollections.find(query).toArray();
+  res.send(result);
 });
 
 app.listen(port, () => {
